fc8fb8d128fb2f819027a5792001bb62f542b5d4
Alexander Ortega <50054527+alexanderortega@users.noreply.github.com>
2020-03-10 12:03:55 -0500

    Prueba Con auth
    Aumenta Archivo de Auth y algunos relacionados

--- api-rest/models/user.js
+++ api-rest/models/user.js
@@ -14,27 +14,32 @@ const UserSchema = new Schema({
   lastLogin: Date
 })
 
-UserSchema.pre('save', (next) => {
-  let user = this
-  if (!user.isMofified('password')) return next()
-
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
   bcrypt.genSalt(10, (err, salt) => {
-    if(err) return next(err)
+    if (err) return next(err)
 
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if(err) return next(err)
-
-      user.password = hash
+    bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if (err) return next(err)
+      this.password = hash
       next()
     })
   })
 })
 
-UserSchema.methods.gravatar = function () {
-  if (!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
-
+UserSchema.methods.gravatar = function (size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) return `https:/gravatar.com/avatar/?s${size}&d=retro`
   const md5 = crypto.createHash('md5').update(this.email).digest('hex')
-  return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`
+}
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch)
+  });
 }
 
 module.exports = mongoose.model('User', UserSchema)
--- api-rest/routes/index.js
+++ api-rest/routes/index.js
@@ -2,7 +2,9 @@
 
 const express = require('express')
 const productCtrl = require('../controllers/product')
+const userCtrl = require('../controllers/user')
 const NodoCtrl = require('../controllers/nodo')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 api.get('/product', productCtrl.getProducts)
@@ -10,7 +12,11 @@ api.get('/product/:productId', productCtrl.getProduct)
 api.post('/product', productCtrl.saveProduct)
 api.put('/product/:productId', productCtrl.updateProduct)
 api.delete('/product/:productId' , productCtrl.deleteProduct)
-
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({ message: 'Tienes Acceso' })
+})
 
 api.get('/nodo/:nodoId', NodoCtrl.getNodo)
 
